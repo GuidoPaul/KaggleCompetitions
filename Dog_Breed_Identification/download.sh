38d95be5d7c496fc4080503a1bb7720e567ea146
williamxie <williamxiewz@gmail.com>
2018-01-08 14:26:58 +0800

    fix bug

---
+++ Dog_Breed_Identification/download.sh
@@ -0,0 +1,17 @@
+#https://www.kaggle.com/product-feedback/21591
+#1 chrome install extension  https://chrome.google.com/webstore/detail/cookiestxt/njabckikapfpffapmjgojcnbfjonfjfg
+#2 login kaggle
+#3 export cookie.txt [# To download cookies for this tab click here, or download all cookies.]
+#4 run this shell
+
+wget -x -c --load-cookies cookies.txt -P input -nH --cut-dirs=5 https://www.kaggle.com/c/dog-breed-identification/download/labels.csv.zip
+
+wget -x -c --load-cookies cookies.txt -P input -nH --cut-dirs=5 https://www.kaggle.com/c/dog-breed-identification/download/sample_submission.csv.zip
+
+wget -x -c --load-cookies cookies.txt -P input -nH --cut-dirs=5 https://www.kaggle.com/c/dog-breed-identification/download/test.zip
+
+wget -x -c --load-cookies cookies.txt -P input -nH --cut-dirs=5 https://www.kaggle.com/c/dog-breed-identification/download/train.zip
+
+unzip -a \*.zip
+rm -f labels.csv.zip sample_submission.csv.zip test.zip train.zip
+
