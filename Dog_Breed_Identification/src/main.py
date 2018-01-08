e0e02b5b9cd6b934c4ee05f54956b413db3b689f
GuidoPaul <Baoshenglin1994@gmail.com>
2018-01-08 22:55:26 +0800

    generate features and create model dir.

--- Dog_Breed_Identification/src/main.py
+++ Dog_Breed_Identification/src/main.py
@@ -17,7 +17,6 @@ from keras.applications.xception import preprocess_input as xception_preprocesso
 from keras.applications.inception_v3 import preprocess_input as inception_v3_preprocessor
 
 
-
 def read_data(labels_path, img_path, use_top_16):
     train_df = pd.read_csv(labels_path)
 
@@ -62,59 +61,55 @@ def generate_features(train_img, train_labels):
     epochs = 30
     # num_classes = len(top_breeds)
     num_classes = 16
-    SEED = 1234
+    SEED = 2018
 
     def get_features(model_info, data, labels, datagen):
         print("generating features...")
         datagen.preprocessing_function = model_info["preprocessor"]
         generator = datagen.flow(
-            data,
-            labels,
-            shuffle=False,
-            batch_size=batch_size,
-            seed=model_info["seed"])
+            data, labels, shuffle=False, batch_size=batch_size, seed=SEED)
         bottleneck_model = model_info["model"](
             weights='imagenet',
             include_top=False,
             input_shape=model_info["input_shape"],
             pooling=model_info["pooling"])
-        return bottleneck_model.predict_generator(generator)
+        return bottleneck_model.predict_generator(
+            generator, steps=(len(data) // batch_size) + 1)
 
     models = {
-        # "InceptionV3": {
-        #     "model": InceptionV3,
-        #     "preprocessor": inception_v3_preprocessor,
-        #     "input_shape": (299, 299, 3),
-        #     "seed": 1234,
-        #     "pooling": "avg"
-        # },
+        "InceptionV3": {
+            "model": InceptionV3,
+            "preprocessor": inception_v3_preprocessor,
+            "input_shape": (224, 224, 3),
+            "pooling": "avg"
+        },
         "Xception": {
             "model": Xception,
             "preprocessor": xception_preprocessor,
-            "input_shape": (299, 299, 3),
-            "seed": 5512,
+            "input_shape": (224, 224, 3),
             "pooling": "avg"
         }
     }
 
     for model_name, model in models.items():
         print("Predicting : {}".format(model_name))
-        filename = model_name + '_features.npy'
-        if os.path.exists(filename):
-            features = np.load(filename)
+        filepath = os.path.join("../model", model_name + '_features.npy')
+        if os.path.exists(filepath):
+            features = np.load(filepath)
         else:
-            train_datagen = ImageDataGenerator(
+            datagen = ImageDataGenerator(
                 zoom_range=0.3, width_shift_range=0.1, height_shift_range=0.1)
-            features = get_features(model, train_img, train_labels,
-                                    train_datagen)
-            np.save(filename, features)
+            features = get_features(model, train_img, train_labels, datagen)
+            np.save(filepath, features)
 
         # Now that we have created or loaded the features  we need to do some predictions.
         start_time = time.time()
 
+        print(features.shape, train_labels.shape)
+
         # logreg = LogisticRegression(
         #     multi_class='multinomial', solver='lbfgs', random_state=SEED)
-        # logreg.fit(features, (y_train * range(num_classes)).sum(axis=1))
+        # logreg.fit(features, (train_labels * range(num_classes)).sum(axis=1))
 
         # model["predict_proba"] = logreg.predict_proba(validation_features)
         end_time = time.time()
@@ -126,7 +121,7 @@ def main():
     train_img, train_labels = read_data('../input/labels.csv',
                                         '../input/train', True)
 
-    show_data(train_img, train_labels)
+    # show_data(train_img, train_labels)
 
     generate_features(train_img, train_labels)
 
